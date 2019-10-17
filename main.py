4760a0570c92ff30840733920305bfa969504429
mullinaxgr2 <mullinaxgr@gmail.com>
2019-10-17 16:05:27 -0400

    added legitimate config and made raid names caps-insensitive

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
-import secret
 from discord.ext import commands
 import gspread
+import configparser
 from datetime import datetime
 from oauth2client.service_account import ServiceAccountCredentials
+
+config = configparser.ConfigParser()
+config.read_file(open('config.ini'))
 
 scope = ["https://spreadsheets.google.com/feeds",
          "https://www.googleapis.com/auth/drive"]
 
-credentials = ServiceAccountCredentials.from_json_keyfile_name("Jorach-5b2d048d0abd.json", scope)
+credentials = ServiceAccountCredentials.from_json_keyfile_name(config["keys"]["GoogleCredentialsFile"], scope)
 gc = gspread.authorize(credentials)
-spreadsheet = gc.open_by_key("1xOtoOyKbesB-EOqKLvfv6cB_oj30pr_LYlFZ_BkuqwU")  # spreadsheet id
+spreadsheet = gc.open_by_key(config["default"]["SpreadsheetId"])
 identity_worksheet = spreadsheet.worksheet("identity")
 
 bot = commands.Bot(command_prefix="!", description="its jorach")
@@ -40,15 +43,20 @@ async def identity(ctx, name: str, wow_class: str, role: str):
     author_hash = str(hash(ctx.author))
 
     if author_hash in discord_ids:
-        print(discord_ids.index(author_hash))
         identity_worksheet.delete_row(discord_ids.index(author_hash) + 1)  # sheets is indexed starting at 1
 
-    identity_worksheet.append_row([hash(ctx.author), author_hash, name, wow_class, role])
+    identity_worksheet.append_row([author_hash, str(ctx.author), name, wow_class, role])
     await ctx.send("your identity has been saved")
 
 
 @bot.command()
 async def register(ctx, raid_name: str):
+    raid_name_lower = raid_name.lower()
+
+    if raid_name_lower == "identity":
+        await ctx.send("nice try")
+        return
+
     discord_ids = identity_worksheet.col_values(1)
     author_hash = str(hash(ctx.author))
 
@@ -58,11 +66,11 @@ async def register(ctx, raid_name: str):
 
     worksheets = spreadsheet.worksheets()
     raid_names = map(lambda ws: ws.title, worksheets)
-    if raid_name not in raid_names:
+    if raid_name_lower not in raid_names:
         await ctx.send("that is not a valid raid! valid raids are: " + str(raid_names))
         return
 
-    raid_worksheet = spreadsheet.worksheet(raid_name)
+    raid_worksheet = spreadsheet.worksheet(raid_name_lower)
     identity_values = identity_worksheet.row_values(discord_ids.index(author_hash) + 1)
 
     name, wow_class, role = (identity_values[2], identity_values[3], identity_values[4])
@@ -75,4 +83,4 @@ async def register(ctx, raid_name: str):
     raid_worksheet.insert_row([name, wow_class, role, str(datetime.now())])
     await ctx.send("you have been signed up")
 
-bot.run(secret.discord_token)
+bot.run(config["keys"]["DiscordSecret"])
