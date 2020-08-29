78b76e6cc71e8bfa98c86cbd716066f7f3bc7f1e
Skippy Ta <skippyta@gmail.com>
2020-08-28 21:09:16 -0700

    These data types suck

--- identity/edit.py
+++ identity/edit.py
@@ -36,7 +36,7 @@ class EditIdentityMenu(EmbedMenu):
         # The user has other profiles; make sure a character with the name exists!
         named_rows = get_rows_with_value_in_column(identity_worksheet, column_index=3,
                                                    value_to_find=name.lower(), list_search_rows=user_rows)
-        data = row_values(named_rows[0])
+        data = row_values(identity_worksheet, named_rows[0])
         wow_class = data[3]
         wow_role = await prompt_choices("What is your role?", user, get_class_roles(wow_class))
         for r in named_rows:
